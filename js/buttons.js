a483f6d45aa2edd5a2643253927942fa5e433a96
Erickson <ericksonvegaf@gmail.com>
2024-09-30 19:42:49 -0300

    Projeto TCC 30/09

---
+++ js/buttons.js
@@ -0,0 +1,9 @@
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
